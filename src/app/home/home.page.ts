5c7b87351a8391980f56296351cc2878d4779c2c
Gustavo <gustavo.27c02@gmail.com>
2021-05-26 13:55:50 -0300

    First commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Artist } from '../models/ArtistSearchResult';
+import { ArtistService } from '../services/artist.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public query = ''
+  public searchValue = ''
+  public artists: Artist[] = []
+
+  constructor(private artistService: ArtistService) { }
+
+  public async performSearch() {
+    this.artists = await this.artistService.searchByName(this.query);
+  }
+
+}
